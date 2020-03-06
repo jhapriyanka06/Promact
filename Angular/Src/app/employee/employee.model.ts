627301e10b3bde425e66f5bf57e4aa553631be65
jhapriyanka06 <61044611+jhapriyanka06@users.noreply.github.com>
2020-03-06 19:13:30 +0530

    Add files via upload

---
+++ Angular/Src/app/employee/employee.model.ts
@@ -0,0 +1,13 @@
+export interface Employee{
+  firstname:string,
+  lastname:string,
+  mail:string,
+  contact:number,
+  address:string,
+  username:string,
+  pwd:string,
+  gender:string,
+  qulaification:string,
+  experience:string,
+  Coding:boolean
+}
